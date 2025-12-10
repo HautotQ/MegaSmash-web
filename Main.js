c26c1582f127bf1dddad9bdd5df759e61e6bdc63
Tabarcraft <166629849+HautotQ@users.noreply.github.com>
2025-12-10 17:02:41 +0100

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,21 +1,9 @@
 import ContentView from "./src/js/views/ContentView.js";
-import { navigationButton } from "./src/js/utils/functions.js";
 
 export default class Main {
     constructor(root) {
         this.root = root; // le conteneur principal
-        const h1 = document.createElement("h1");
-        h1.innerText = "Texte Main.js";
-        this.root.appendChild(h1);
-
-        const testBtn = navigationButton(
-            this.root,
-            "Test",
-            ContentView
-        );
-
-        
-        this.root.appendChild(testBtn);
+        new ContentView(this.root);
     }
 }
 
