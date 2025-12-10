7bdb5fb0c71033171b953113bef070d8747e2e39
Tabarcraft <166629849+HautotQ@users.noreply.github.com>
2025-12-10 17:04:10 +0100

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,15 +1,6 @@
 import ContentView from "./src/js/views/ContentView.js";
 
-export default class Main {
-    constructor(root) {
-        this.root = root; // le conteneur principal
-        new ContentView(this.root);
-    }
-}
-
-// Lance Main
-
 window.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    new Main(root);
+    new ContentView(root);
 });
