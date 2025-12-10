8b79b873b656b921c48163e3d29ae17f82bfb39e
Tabarcraft <166629849+HautotQ@users.noreply.github.com>
2025-12-10 16:23:13 +0100

    Update functions.js

--- src/js/utils/functions.js
+++ src/js/utils/functions.js
@@ -1,6 +1,6 @@
 function navigationButton(root, label, ViewClass, ...args) {
     const btn = document.createElement("button");
     btn.innerText = label;
-    btn.onclick = () => new ViewClass(...args).show(root);
+    btn.onclick = () => new ViewClass(root, ...args);
     return btn;
 }
