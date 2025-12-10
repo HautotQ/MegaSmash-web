aa7f3c806c6f2151ba633b8cf98ebe1bde8dd64d
Tabarcraft <166629849+HautotQ@users.noreply.github.com>
2025-12-10 16:14:05 +0100

    Update Main.js

--- Main.js
+++ Main.js
@@ -3,7 +3,11 @@ import ContentView from "./src/js/views/ContentView.js";
 class Main {
     constructor(root) {
         this.root = root; // le conteneur principal
-        new ContentView();
+        const h1 = document.createElement("h1");
+        h1.innerText = "Texte Main.js";
+        this.root.appendChild(h1);
+        
+        //new ContentView();
     }
 }
 
