8c2c5632257bce9e7408de9dfab1a8508b4ea178
Tabarcraft <166629849+HautotQ@users.noreply.github.com>
2025-12-10 16:30:54 +0100

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,7 +1,7 @@
 import ContentView from "./src/js/views/ContentView.js";
 import { navigationButton } from "./src/js/utils/functions.js";
 
-class Main {
+export default class Main {
     constructor(root) {
         this.root = root; // le conteneur principal
         const h1 = document.createElement("h1");
