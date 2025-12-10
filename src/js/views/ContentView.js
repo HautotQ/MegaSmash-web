349198a6a9678087eb396508f06dee690ebe3bb5
Tabarcraft <166629849+HautotQ@users.noreply.github.com>
2025-12-10 16:38:07 +0100

    Update ContentView.js

--- src/js/views/ContentView.js
+++ src/js/views/ContentView.js
@@ -3,8 +3,16 @@ import { navigationButton } from "../utils/functions.js";
 
 export default class ContentView {
   constructor(root) {
+    this.root = root;
+    
     const h2 = document.createElement("h2");
     h2.innerText = "Test";
     root.appendChild(h2);
+
+    const backBtn = navigationButton(
+      this.root,
+      "Retour",
+      Main
+    );
   }
 }
