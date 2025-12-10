2cb26f1d45bf0f2847b2acbc4911f8007d1d7c4c
Tabarcraft <166629849+HautotQ@users.noreply.github.com>
2025-12-10 16:18:25 +0100

    Update ContentView.js

--- src/js/views/ContentView.js
+++ src/js/views/ContentView.js
@@ -2,6 +2,10 @@
 
 export default class ContentView {
   constructor(root) {
-    
+    this.root = root;
+
+    const h2 = document.createElement("h2");
+    h2.innerText = "Test";
+    root.appendChild(h2);
   }
 }
